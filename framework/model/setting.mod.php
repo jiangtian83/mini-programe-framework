f83148359088fcac55a54312972590106b6fdd76
xinxin <97675504@qq.com>
2019-11-11 13:54:23 +0800

    t

--- framework/model/setting.mod.php
+++ framework/model/setting.mod.php
@@ -28,7 +28,9 @@ function setting_load($key = '') {
 	global $_W;
 
 	$cachekey = cache_system_key('setting');
+	echo 9;
 	$settings = cache_load($cachekey);
+	echo 0;
 	if (empty($settings)) {
 		$settings = pdo_fetchall('SELECT * FROM ' . tablename('core_settings'), array(), 'key');
 		if (is_array($settings)) {
@@ -38,6 +40,7 @@ function setting_load($key = '') {
 		}
 		cache_write($cachekey, $settings);
 	}
+	echo 1;die;
 	if (!is_array($_W['setting'])) {
 		$_W['setting'] = array();
 	}
