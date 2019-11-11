1dbfc7db7d5717e28ba2c8552ac62b5c7cb072de
xinxin <97675504@qq.com>
2019-11-11 13:52:28 +0800

    t

--- framework/bootstrap.inc.php
+++ framework/bootstrap.inc.php
@@ -130,7 +130,6 @@ $_GPC = ihtmlspecialchars($_GPC);
 
 // 拼接访问url
 $_W['siteurl'] = $urls['scheme'].'://'.$urls['host'].((!empty($urls['port']) && $urls['port']!='80') ? ':'.$urls['port'] : '') . $_W['script_name'] . '?' . http_build_query($_GET, '', '&');
-echo 8888;die;
 // 非ajax请求，从php://input获取值
 if (!$_W['isajax']) {
 	$input = file_get_contents("php://input");
@@ -143,7 +142,7 @@ if (!$_W['isajax']) {
 	}
 	unset($input, $__input);
 }
-
+echo 9999;die;
 setting_load();
 if (empty($_W['setting']['upload'])) {
     // 仅向 array_merge() 函数输入一个数组，且键名是整数，则该函数将返回带有整数键名的新数组，其键名以 0 开始进行重新索引
